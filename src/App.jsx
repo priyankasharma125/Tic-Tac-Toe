4ae97b31a2b717f73363afab81eb50807d088c24
Priyanka <priyankasharma79888@gmail.com>
2023-08-08 13:16:42 +0530

    add winner and next player

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./style.scss";
+import { useState } from "react";
+import { calculatewinner } from "./components/winner";
+import Board from "./components/Board.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [square, setSquare] = useState(Array(9).fill(null));
+  const [isXturn, setXturn] = useState(true);
+  //   console.log(square);
+  const winner = calculatewinner(square);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXturn ? "X" : "O"}`;
+  const handleClick = (clickedPos) => {
+    if (square[clickedPos] || winner) {
+      return;
+    }
+    setSquare((clickedSquare) => {
+      return clickedSquare.map((squareVal, index) => {
+        if (clickedPos === index) {
+          return isXturn ? "X" : "O";
+        }
+        return squareVal;
+      });
+    });
+    setXturn((currentIsXturn) => !currentIsXturn);
+    // console.log("clicked at", index);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <h2>{message}</h2>
+        <Board square={square} handleClick={handleClick} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Board.jsx
@@ -0,0 +1,28 @@
+import Square from "./Square";
+
+const Board = ({ square, handleClick }) => {
+  const renderSquare = (index) => {
+    return <Square value={square[index]} onClick={() => handleClick(index)} />;
+  };
+  return (
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
